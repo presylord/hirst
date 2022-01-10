be010191cc5a7ec281fa7f3c9f584afa7cdf479e
presylord <kidlat123@gmail.com>
2022-01-10 13:14:46 +0800

    update

--- main.py
+++ main.py
@@ -1,19 +1,44 @@
 import colorgram
+import random
 from turtle import Screen, Turtle
 
+x = 10
+y = 10
+
 turtle = Turtle()
+turtle.ht()
+turtle.width(10)
+turtle.speed("fastest")
+
 screen = Screen()
 screen.colormode(255)
 
-num_color = 10
+num_color = 30
 colors = colorgram.extract('danielhirstpainting.jpg', num_color)
+colors_list = []
 
 
-for _ in range(num_color):
-    rgb = colors[_].rgb
-    r = rgb.r
-    g = rgb.g
-    b = rgb.b
+def generate_color():
+    for _ in range(num_color):
+        rgb = colors[_].rgb
+        color = (rgb.r, rgb.g, rgb.b)
+        colors_list.append(color)
+
+
+def dot_x():
+    for axis_x in range(int(x)):
+        turtle.dot(20, random.choice(colors_list))
+        turtle.penup()
+        turtle.forward(50)
+
+
+generate_color()
+turtle.penup()
+
+for _ in range(int(y)):
+    turtle.setpos(-200, -200 + (_ * 50))
+    dot_x()
+
 
 
 
